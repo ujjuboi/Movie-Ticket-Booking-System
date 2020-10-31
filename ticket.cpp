8b4e5f2bc036cc3ae053c5276289b5190d2e0fa1
ujjuboi <dev@LAPTOP-HC8BJ4AO.localdomain>
2020-11-01 00:14:06 +0530

    Fixed Bugs after testing
    2 bugs still need to be fixed (check comments)

--- ticket.cpp
+++ ticket.cpp
@@ -7,8 +7,7 @@ using namespace std;
 class Booking{
     private:
     
-        char name[100], email[100], upi[100], confirm[100] = "no";
-        int mobile = 0;
+        char name[100], email[100], upi[100], confirm[7], mobile[10];
 
         void customerInfo(){
             //gather customer info:
@@ -16,8 +15,12 @@ class Booking{
             cin.getline(name, 100);
             cout<<endl<<"Please enter your mobile number: ";
             cin>>mobile;
-            cout<<endl<<"Please enter your email number: ";
-            cin.getline(email, 100);
+            cout<<endl<<"Please enter your Email ID: ";
+            cin>>email;
+            if(strstr(email, "@") <= 0 ) {
+                cout<<endl<<"Please enter a valid Email ID: ";
+                cin>>email;
+            }
         }
 
         int payment(){
@@ -27,7 +30,16 @@ class Booking{
             cout<<"Mobile Number: "<<mobile<<endl;
             cout<<"Email ID: "<<email<<endl;
             cout<<"Please type in 'correct' to confirm you details are correct else type 'no': ";
-            cin>>confirm;
+            //Exception in turn of spelling:
+            try{
+                cin>>confirm;
+                //error here*
+                throw "Incorrect input, please check the spelling again.";
+            }
+            catch(const char* error){
+                cout<<error<<endl;
+                cin>>confirm;
+            }
             //confirm details:
             if(strcmp(confirm,"no") == 0){
                 cout<<"Please reenter your information again: "<<endl;
@@ -36,13 +48,11 @@ class Booking{
             else if(strcmp(confirm, "correct") == 0){
                 cout<<"Please enter you UPI ID: ";
                 cin>>upi;
-                if(strstr(email, "@") < 0)
-                    return 0;
-                else
+                if(strstr(upi, "@") > 0)
                     return 1;
-            }
+           }
+           return 0;
         }
-
     public:
         //display menu, starting point: 
         void menu(){
@@ -56,10 +66,23 @@ class Booking{
             }
             movielist.close();
         }
+        //after selecting movie of choice:
+        void selection(){
+            customerInfo();
+            if(payment() == 1){
+                cout<<"Payment Done";
+                //print full details
+            }
+            else{
+                cout<<"Payment Failed, please retry after some time";
+                //redirect to main*
+            }
+        }
 }book;
 
 int main(){
     system("clear");
     book.menu();
+    book.selection();
     return 0;
 }
